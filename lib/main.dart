b223919d8272459b29998de2f6fc07014e20436d
Gadiel Sosa <sosachanantonio0@gmail.com>
2025-03-26 18:53:15 -0600

    Starting Chat App

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app_yes_no/config/theme/app_theme.dart';
+import 'package:chat_app_yes_no/presentation/chat/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 6).theme(),
+      home: const ChatScreen(),
+    );
+  }
+}
